87057788a6aa8735cc4337922954bc2bd5b879ea
LigiaNasc <ligianascimento002@gmail.com>
2024-05-22 10:22:17 -0300

    contrato

---
+++ contrato.js
@@ -0,0 +1,21 @@
+function funcao1() {
+    var x = document.getElementById("nome").value;
+    document.getElementById("demo").innerHTML = x;
+  }
+
+function funcao2() {
+    var rg = document.getElementById("rg").value;
+    document.getElementById("srg").innerHTML = rg;
+  }
+  function funcao3() {
+    var cpf = document.getElementById("cpf").value;
+    document.getElementById("scpf").innerHTML = cpf;
+  }
+  function funcao4() {
+    var pf = document.getElementById("profissao").value;
+    document.getElementById("spf").innerHTML = pf;
+  }
+  function funcao5() {
+    var sed = document.getElementById("endereço").value;
+    document.getElementById("sed").innerHTML = sed;
+  }
